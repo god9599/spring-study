1aee1cd955f9fab4ecb5e9bc8117c925274eec71
god9599 <59276134+god9599@users.noreply.github.com>
2021-06-17 22:57:17 +0900

    쿼리스트링 사용

--- src/main/java/hello/hellospring/controller/hello/HelloController.java
+++ src/main/java/hello/hellospring/controller/hello/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller.hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +11,11 @@ public class HelloController {
     public String hello(Model model){
     model.addAttribute("data", "hello!!");
     return "hello"; // template 폴더에 hello 파일을 찾아 렌더링 -> 컨트롤러에서 리턴 값으로 문자를 반환하면 viewResolver가 화면을 찾아서 처리
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
