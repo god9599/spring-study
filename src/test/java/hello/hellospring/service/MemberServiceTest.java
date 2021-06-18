2ba53c5f56c34b051d177514f68d714cdefdfd43
god9599 <59276134+god9599@users.noreply.github.com>
2021-06-18 13:19:27 +0900

    테스트 코드 리팩토링

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class MemberServiceTest {
@@ -29,18 +30,20 @@ class MemberServiceTest {
         //given
         Member member1 = new Member();
         member1.setName("spring");
-
+ 
         Member member2 = new Member();
         member2.setName("spring");
 
         //when
         memberService.join(member1);
-        try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e){
-            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름입니다.");
-        }
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름입니다.");
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e){
+//            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름입니다.");
+//        }
         //then
     }
 
