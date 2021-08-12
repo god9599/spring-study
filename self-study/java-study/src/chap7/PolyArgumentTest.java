d768a6b699a9da1c36772c98931366331d0aa6cc
god9599 <tjdals3415@gmail.com>
2021-08-12 11:05:07 +0900

    Refact: 객체를 배열로 다루기

--- self-study/java-study/src/chap7/PolyArgumentTest.java
+++ self-study/java-study/src/chap7/PolyArgumentTest.java
@@ -32,7 +32,8 @@ class Computer1 extends Product {
 class Buyer {
     int money = 1000;
     int bonusPoint = 0;
-
+    Product[] item = new Product[10];
+    int i = 0;
     void buy(Product product) {
         if (money < product.price) {
             System.out.println("잔고 부족이요");
@@ -40,7 +41,23 @@ class Buyer {
         }
         money -= product.price;
         bonusPoint += product.bonusPoint;
+        item[i++] = product;
         System.out.println(product + "를 구매했습니다");
+    }
+    void summary() {
+        int sum = 0;
+        String itemList = "";
+
+        // 구입한 물품의 총 가격과 목록을 만든다.
+        for (int i = 0; i < item.length; i++) {
+            if (item[i] == null) {
+                break;
+            }
+            sum += item[i].price;
+            itemList += item[i] + ", ";
+        }
+        System.out.println("구입한 물품의 총 금액은 : " + sum + "원 입니다");
+        System.out.println("구입한 물품들은" + itemList + "입니다");
     }
 }
 public class PolyArgumentTest {
@@ -50,5 +67,6 @@ public class PolyArgumentTest {
         buyer.buy(new Computer1());
         System.out.println("남은 돈 : " + buyer.money + "만원");
         System.out.println("보너스 포인트 : " + buyer.bonusPoint + "점");
+        buyer.summary();
     }
 }
