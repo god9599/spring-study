6305c4abe07f240732509aa904bd11f2d655614c
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-08-17 15:52:53 +0900

    [Etc] 트랜잭션 설정

--- board/src/main/java/com/example/board/board/service/BoardServiceImpl.java
+++ board/src/main/java/com/example/board/board/service/BoardServiceImpl.java
@@ -4,10 +4,12 @@ import com.example.board.board.dto.BoardDto;
 import com.example.board.board.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class BoardServiceImpl implements BoardService{
     @Autowired
     private BoardMapper boardMapper;
