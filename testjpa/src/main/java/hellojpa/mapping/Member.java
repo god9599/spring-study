e107bfce4a442677865c72e49b35e150ccf9b2cc
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-09-16 15:42:53 +0900

    엔티티 매핑 : PK 매핑

--- testjpa/src/main/java/hellojpa/mapping/Member.java
+++ testjpa/src/main/java/hellojpa/mapping/Member.java
@@ -9,8 +9,27 @@ import java.util.Date;
 @Table(uniqueConstraints = {
         @UniqueConstraint(name = "name", columnNames = {"name"}),
         @UniqueConstraint(name = "age", columnNames = {"age"})})
+/*
+* 미리 50개의 id 값을 불러오고 메모리에서 사용 50번까지 쓰면 미리 +50개씩 생성
+* allocationSize를 설정하지 않으면 계속해서 네트워크(데이터베이스에 다음 id값 내놓으라고 요청하기 때문)를 타야하므로 성능 최적화에 사용된다.
+* SEQUENCE 전략은 영속성 commit시에 SQL이 날라감
+* */
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1,
+        allocationSize = 50
+)
 public class Member {
-    @Id
+    @Id // Id를 직접 할당해야 함
+    /*
+     * IDENTITY 전략에서는 persist하는 시점에서 바로 SQL이 DB로 날라간다.
+     * WHY? AUTO_INCREMENT는 데이터베이스에 insert SQL을 실행한 이후에 ID값을 알 수 있다. 그러면 영속성 컨텍스트에서 관리를 커밋하기 전까지 할 수가 없음
+     * THEN. 모아서 SQL을 날리는 행위는 할 수 없음.
+     *
+     @GeneratedValue(strategy = GenerationType.IDENTITY) // 디비에 Id 생성 위임
+     * */
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name")
